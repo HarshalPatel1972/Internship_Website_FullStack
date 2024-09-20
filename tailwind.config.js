05c5be780bd5f255c118acc3fb9b136bb891c1d3
Harshal Patel <106813066+HarshalPatel1972@users.noreply.github.com>
2024-09-20 20:08:06 +0530

    Adding main files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: "class", // Enable dark mode class
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        darkGray: "#1f2937",
+        darkBlue: "#1e3a8a",
+        lightGray: "#f9fafb",
+      },
+    },
+  },
+  plugins: [],
+};
